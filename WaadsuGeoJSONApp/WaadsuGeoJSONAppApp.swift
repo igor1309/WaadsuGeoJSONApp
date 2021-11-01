aab96b60c68e915c8bab6c0033e8245288252144
Igor Malyarov <igor1309@gmail.com>
2021-11-01 13:54:33 +0300

    Initial Commit

---
+++ WaadsuGeoJSONApp/WaadsuGeoJSONAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaadsuGeoJSONAppApp.swift
+//  WaadsuGeoJSONApp
+//
+//  Created by Igor Malyarov on 01.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct WaadsuGeoJSONAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
